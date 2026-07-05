aa627a5c625474f8600b99158c0b966aef655a59
ayyushhtf <ayyushhtf@gmail.com>
2026-07-05 17:26:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function login(){
 
+let pass=document.getElementById("password").value;
+
+if(pass=="Ayush"){
+alert("Login Successful");
+}else{
+document.getElementById("msg").innerHTML="Wrong Password";
+}
+
+}
